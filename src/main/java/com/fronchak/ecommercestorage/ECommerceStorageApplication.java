e3d4166448cf2d193191e578b59f02a1500b3e03
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-02-17 18:18:30 -0300

    Project created

---
+++ src/main/java/com/fronchak/ecommercestorage/ECommerceStorageApplication.java
@@ -0,0 +1,13 @@
+package com.fronchak.ecommercestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fronchak/ecommercestorage/ECommerceStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fronchak.ecommercestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
