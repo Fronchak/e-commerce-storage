7ee05cced375ac5b670512950f6eb83775017d3f
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-02-18 11:53:47 -0300

    Implemented unit tests to SupplierService

--- src/main/java/com/fronchak/ecommercestorage/services/SupplierService.java
+++ src/main/java/com/fronchak/ecommercestorage/services/SupplierService.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.fronchak.ecommercestorage.dtos.supplier.SupplierInsertDTO;
 import com.fronchak.ecommercestorage.dtos.supplier.SupplierOutputDTO;
+import com.fronchak.ecommercestorage.dtos.supplier.SupplierUpdateDTO;
 import com.fronchak.ecommercestorage.entities.Supplier;
+import com.fronchak.ecommercestorage.exceptions.ResourceNotFoundException;
 import com.fronchak.ecommercestorage.mappers.SupplierMapper;
 import com.fronchak.ecommercestorage.repositories.SupplierRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class SupplierService {
@@ -20,12 +26,34 @@ public class SupplierService {
 	private SupplierMapper mapper;
 	
 	public SupplierOutputDTO findById(Long id) {
-		Supplier entity = repository.findById(id).get();
+		Supplier entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Supplier", id.toString()));
 		return mapper.convertSupplierEntityToSupplieroutputDTO(entity);
 	}
 	
 	public List<SupplierOutputDTO> findAll() {
 		List<Supplier> entityList = repository.findAll();
 		return mapper.convertSupplierEntityListToSupplierOutputDTOList(entityList);
+	}
+	
+	@Transactional
+	public SupplierOutputDTO save(SupplierInsertDTO dto) {
+		Supplier entity = new Supplier();
+		mapper.copySupplierDTOToSupplierEntity(entity, dto);
+		entity = repository.save(entity);
+		return mapper.convertSupplierEntityToSupplieroutputDTO(entity);
+	}
+	
+	@Transactional
+	public SupplierOutputDTO update(SupplierUpdateDTO dto, Long id) {
+		try {
+			Supplier entity = repository.getReferenceById(id);
+			mapper.copySupplierDTOToSupplierEntity(entity, dto);
+			entity = repository.save(entity);
+			return mapper.convertSupplierEntityToSupplieroutputDTO(entity);	
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Supplier", id.toString());
+		}
 	}
 }
--- src/test/java/com/fronchak/ecommercestorage/services/SupplierServiceTest.java
+++ src/test/java/com/fronchak/ecommercestorage/services/SupplierServiceTest.java
@@ -1,6 +1,10 @@
 package com.fronchak.ecommercestorage.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -11,17 +15,23 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.fronchak.ecommercestorage.dtos.supplier.SupplierInputDTO;
+import com.fronchak.ecommercestorage.dtos.supplier.SupplierInsertDTO;
 import com.fronchak.ecommercestorage.dtos.supplier.SupplierOutputDTO;
+import com.fronchak.ecommercestorage.dtos.supplier.SupplierUpdateDTO;
 import com.fronchak.ecommercestorage.entities.Supplier;
 import com.fronchak.ecommercestorage.exceptions.ResourceNotFoundException;
 import com.fronchak.ecommercestorage.mappers.SupplierMapper;
 import com.fronchak.ecommercestorage.repositories.SupplierRepository;
 import com.fronchak.ecommercestorage.test.factories.SupplierMocksFactory;
 import com.fronchak.ecommercestorage.test.util.CustomAsserts;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class SupplierServiceTest {
@@ -32,6 +42,9 @@ public class SupplierServiceTest {
 	
 	private Supplier entity;
 	private List<Supplier> entityList;
+	
+	private SupplierInsertDTO insertDTO;
+	private SupplierUpdateDTO updateDTO;
 	
 	@Mock
 	private SupplierRepository repository;
@@ -46,15 +59,23 @@ public class SupplierServiceTest {
 	void setUp() {
 		entity = SupplierMocksFactory.mockSupplierEntity();
 		entityList = SupplierMocksFactory.mockSupplierEntityList();
+		
 		SupplierOutputDTO outputDTO = SupplierMocksFactory.mockSupplierOutputDTO();
 		List<SupplierOutputDTO> outputDTOList = SupplierMocksFactory.mockSupplierOutputDTOList();
+		
+		insertDTO = SupplierMocksFactory.mockSupplierInsertDTO();
+		updateDTO = SupplierMocksFactory.mockSupplierUpdateDTO();
 		
 		when(repository.findById(VALID_ID)).thenReturn(Optional.of(entity));
 		when(repository.findById(INVALID_ID)).thenReturn(Optional.empty());
 		when(repository.findAll()).thenReturn(entityList);
+		when(repository.save(any(Supplier.class))).thenReturn(entity);
+		when(repository.getReferenceById(VALID_ID)).thenReturn(entity);
+		when(repository.getReferenceById(INVALID_ID)).thenThrow(EntityNotFoundException.class);
 		
 		when(mapper.convertSupplierEntityToSupplieroutputDTO(entity)).thenReturn(outputDTO);
 		when(mapper.convertSupplierEntityListToSupplierOutputDTOList(entityList)).thenReturn(outputDTOList);
+		doNothing().when(mapper).copySupplierDTOToSupplierEntity(any(Supplier.class), any(SupplierInputDTO.class));
 	}
 	
 	@Test
@@ -76,5 +97,40 @@ public class SupplierServiceTest {
 		List<SupplierOutputDTO> resultList = service.findAll();
 		
 		CustomAsserts.assertSupplierOutputDTOList(resultList);
+	}
+	
+	@Test
+	public void saveShouldReturnSupplierOutputDTOAfterSaveNewSupplier() {
+		ArgumentCaptor<Supplier> argumentCaption = ArgumentCaptor.forClass(Supplier.class);
+		SupplierOutputDTO result = service.save(insertDTO);
+		
+		CustomAsserts.assertSupplierOutputDTO(result);
+		verify(repository).save(argumentCaption.capture());
+		Supplier entitySaved = argumentCaption.getValue();
+		assertNull(entitySaved.getId());
+		assertNull(entitySaved.getName());
+	}
+	
+	@Test
+	public void updateShouldReturnSupplierOutputDTOWhenIdExists() {
+		ArgumentCaptor<Supplier> argumentCaption = ArgumentCaptor.forClass(Supplier.class);
+		SupplierOutputDTO result = service.update(updateDTO, VALID_ID);
+		
+		verify(repository, times(1)).getReferenceById(VALID_ID);
+		verify(mapper, times(1)).copySupplierDTOToSupplierEntity(entity, updateDTO);
+		verify(repository).save(entity);
+		verify(mapper, times(1)).convertSupplierEntityToSupplieroutputDTO(entity);
+		verify(repository).save(argumentCaption.capture());
+		
+		Supplier entityUpdated = argumentCaption.getValue();
+		assertEquals(0L, entityUpdated.getId());
+		assertEquals("Mock supplier name 0", entityUpdated.getName());
+		
+		CustomAsserts.assertSupplierOutputDTO(result);
+	}
+	
+	@Test
+	public void updateShouldReturnResourceNotFoundExceptionWhenIdDoesNotExist() {
+		assertThrows(ResourceNotFoundException.class, () -> service.update(updateDTO, INVALID_ID));
 	}
 }
